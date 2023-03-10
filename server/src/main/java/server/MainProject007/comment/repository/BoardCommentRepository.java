d3362385b74f56860dd174f5fe92664aec79ccc5
lKori <yuy0841@gmail.com>
2023-03-10 13:37:13 +0900

    feat:comment 파일 제외

--- server/src/main/java/server/MainProject007/comment/repository/BoardCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package server.MainProject007.comment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import server.MainProject007.comment.entity.BoardComment;
-
-public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
-}

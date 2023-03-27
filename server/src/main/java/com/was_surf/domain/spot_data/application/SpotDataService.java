d1ec8594a1d18381f31f02478b0fa5496cb2d021
puuruun <puuruun@gmail.com>
2023-03-27 23:27:19 +0900

    chore: 날씨 데이터 관련 코드 수정 필요3

--- server/src/main/java/com/was_surf/domain/spot_data/application/SpotDataService.java
+++ server/src/main/java/com/was_surf/domain/spot_data/application/SpotDataService.java
@@ -18,7 +18,7 @@ public class SpotDataService {
     @Scheduled(cron = "0 0 0/3 1/1 * *")
     public void runEveryTenSecondsOne() {
 
-        Region region = Region.getTemp();
+//        Region region = Region.getTemp();
 
 
 //        log.info("3시간에 한번 : " + LocalTime.now());

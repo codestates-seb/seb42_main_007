1b3a01dabad41ba012bf68450a6cdb44b86a2504
puuruun <puuruun@gmail.com>
2023-03-28 17:13:55 +0900

    chore: 날씨 데이터 기반 추천순 정렬 구현 완료(수정 중)

--- server/src/main/java/com/was_surf/domain/spot_data/application/SpotDataService.java
+++ server/src/main/java/com/was_surf/domain/spot_data/application/SpotDataService.java
@@ -23,45 +23,24 @@ public class SpotDataService {
     private final RegionRepository regionRepository;
     private final SpotDataRepository spotDataRepository;
 
-    // 일정시간마다 데이터 가져오기
-    //cron 표현식, fixedDelay, fixedRate 등 실행 주기 옵션 필수
-    //return 타입이 void여야 함 / parameter를 줄 수 없음
-
-//    @Scheduled(cron = "0 0 0/3 1/1 * *")
-//    public void runEveryTenSecondsOne() {
-//
-////        Region region = Region.getTemp();
-//
-////        log.info("3시간에 한번 : " + LocalTime.now());
-////        log.info("thread: " + Thread.currentThread().getName());
-////        log.info("==============================================");
-//
-//        // 여기서 온도 / 파고 / 풍속 데이터 3시간에 한번 받아오기
-//
-//        // if 문 넣고
-//        // repository.save 하기
-//
-//    }
-
-    @Scheduled(fixedRate = 10000L)
-    public void runEveryTenSecondsTwo() {
+    // 3시간에 한번씩 업데이트(API 제공 시간에서 1분 뒤)
+    @Scheduled(cron = "0 11 2-23/3 1/1 * *")
+    public void runEveryTenSecondsOne() {
 
         for (long i=1; i<=8; i++) {
             SpotData spotData = updateSpotData(i);
             spotDataRepository.save(spotData);
-        }
 
-        log.info("10.000L 10초에 한번 : " + LocalTime.now());
+        log.info("3시간에 한번: " + LocalTime.now());
         log.info("thread: " + Thread.currentThread().getName());
         log.info("==============================================");
+        }
     }
-
 
     public SpotData updateSpotData (Long id) {
         Optional<SpotData> optionalSpotData = spotDataRepository.findById(id);
         SpotData findSpotData = optionalSpotData.orElseThrow(() -> new BusinessLogicException(ExceptionCode.SPOTDATA_NOT_FOUND));
 
-        // Optional: 값이 있을 수도 있고 null일 수도 있음
         Optional<Region> optionalRegion = regionRepository.findById(id);
         Region findRegion = optionalRegion.orElseThrow(() -> new BusinessLogicException(ExceptionCode.SPOTDATA_NOT_FOUND));
 
@@ -75,44 +54,41 @@ public class SpotDataService {
         return findSpotData;
     }
 
+    // 온도
     public int classifyTempScore (Float temp) {
-        if ( temp<=10 ) {
+        if ( temp<=10 ) { // 10도 이하
             return 0;
-        } else if (10<temp && temp<=12) {
+        } else if (10<temp && temp<=12) { // 11~12도
             return 1;
-        } else if (12<temp && temp<=14) {
+        } else if (12<temp && temp<=14) { // 13~14도
             return 2;
-        } else if (14<temp && temp<=17) {
+        } else if (14<temp && temp<=17) { // 15~17도
             return 3;
-        } else if (17<temp && temp<=21) {
+        } else if (17<temp && temp<=21) { // 18~21도
             return 4;
-        } else {
+        } else { // 22도 이상
             return 5;
         }
     }
 
+    // 파고
     public int classifyWaveScore (Float wave) {
-        if (wave<=0.3) {
+        if (wave<=0.3) { // 0.3m 이하
             return 0;
-        } else if (0.3<wave && wave<=0.8) {
+        } else if (0.3<wave && wave<=0.8) { // 0.4~0.8m
             return 1;
-        } else if (0.8<wave && wave<=1.4) {
-            return 3;
-        } else return 2;
+        } else if (0.8<wave && wave<=1.4) { // 0.9~1.4m
+            return 3; // 난이도 분류가 없기 때문에 제일 무난한 코스가 점수 높음
+        } else return 2; // 1.5m 이상
     }
 
+    // 풍속
     public int classifyWindSpeedScore (Float windSpeed) {
-        if (windSpeed<=5) {
+        if (windSpeed<=2.57) { // 5knots = 2.57m/s 이하
             return 2;
-        } else if (5<windSpeed && windSpeed<=14) {
+        } else if (2.57<windSpeed && windSpeed<=7.20) { // ~ 14knots = 7.20m/s
             return 1;
-        } else return 0;
+        } else return 0; // 15knots 이상
     }
-
-
-
 }
-
-
-
 
